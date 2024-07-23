35cce15537417b9b21e309cf195ecb517bd67e22
satoshiasHere <skinyanjui100@gmail.com>
2024-07-23 23:28:50 +0300

    Node Js for evevrything

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+/**
+ * Prints message no the stdout
+ */
+
+const displayMessage = (d) => console.log(d);
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,16 @@
+i/**
+ * Basic server for Express
+ */
+
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port);
+
+module.exports = app;
